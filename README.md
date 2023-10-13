8560d24b0d54bbb59049d7ef87c621d04644e681
halfminami <129936969+halfminami@users.noreply.github.com>
2023-10-13 22:04:18 +0900

    add implementation pseudo codes

--- README.md
+++ README.md
@@ -6,11 +6,11 @@ Maze generation animation with Prim's algorithm, Kruskal's algorithm and DFS.
 
 ## Description
 
-Maze can be generated with spanning tree using graph nodes representing grid. [Maze Generation Algorithm - wikipedia](https://en.wikipedia.org/wiki/Maze_generation_algorithm)
+A maze can be generated with spanning tree using graph nodes representing grid. [Maze Generation Algorithm - wikipedia](https://en.wikipedia.org/wiki/Maze_generation_algorithm)
 
-Some algorithms to find minimum spanning tree are Prim's algorithm and Kruskal's algorithm. This program animates maze generation with the two. You can see the difference.
+Some algorithms to find minimum spanning tree include Prim's algorithm and Kruskal's algorithm. This program animates maze generation with the two. You can see the difference.
 
-Additionally, it contains creating spanning tree maze with no loop DFS.
+Additionally, it contains creating spanning tree maze with no loop random DFS.
 
 ## Installation
 
@@ -24,6 +24,7 @@ npm run start
 ... will start create-react-app's development server.
 
 ## Uses
+
 - create-react-app
 
 ## Reference
--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,71 @@ import { DFSGen, kruskalGen, primGen } from './generation/spanningTree';
 import WrapMaze from './components/WrapMaze';
 import { Container } from 'react-bootstrap';
 
+const pseudoPrim = `func <span class="k">Prim</span>(<span class="k">edges</span> as List of Edge) {
+  <span class="k">addedNodes</span> as Set of Node
+  <span class="k">currentEdges</span> as Set of Edge sorted by weight
+  <span class="k">result</span> as List of Edge
+
+  <span class="d">Add</span> an arbitrary node to <span class="k">addedNodes</span>
+  <span class="d">Add</span> edges connecting to the node to <span class="k">currentEdges</span>
+
+  while (<span class="k">addedNodes</span> does not contain all nodes) {
+    for (each <span class="k">edge</span> of <span class="k">currentEdges</span>) {
+      if (<span class="k">edge</span> connects to a <span class="k">newNode</span> not in <span class="k">addedNodes</span>) {
+        <span class="d">Add</span> <span class="k">newNode</span> to <span class="k">addedNodes</span>
+        <span class="d">Add</span> edges connecting to <span class="k">newNode</span> to <span class="k">currentEdges</span>
+
+        <span class="d">Add</span> <span class="k">edge</span> to <span class="k">result</span>
+        break <span class="c">// Loop over once currentEdges is updated</span>
+      }
+    }
+  }
+  
+  return <span class="k">result</span>
+}`;
+
+const pseudoKruskal = `func <span class="k">Kruskal</span>(<span class="k">edges</span> as List of Edge) {
+  <span class="k">forest</span> as DisjointSet of Node
+  <span class="k">result</span> as List of Edge
+
+  <span class="d">Sort</span> <span class="k">edges</span> by weight
+
+  for (each <span class="k">edge</span> of <span class="k">edges</span>) {
+    if (<span class="k">nodes</span> of <span class="k">edge</span> are not in the same tree in <span class="k">forest</span>) {
+      <span class="d">Add</span> <span class="k">nodes</span> to <span class="k">forest</span>
+
+      <span class="d">Add</span> <span class="k">edge</span> to <span class="k">result</span>
+    }
+  }
+
+  return <span class="k">result</span>
+}`;
+
+const pseudoDFS = `func <span class="k">DFS</span>(<span class="k">edges</span> as List of Edge) {
+  <span class="k">visitedNodes</span> as Set of Node
+  <span class="k">stack</span> as Stack of Node
+  <span class="k">result</span> as List of Edge
+
+  <span class="d">Push</span> an arbitrary node onto <span class="k">stack</span>
+
+  while (<span class="k">stack</span> is not empty) {
+    <span class="k">current</span> <- <span class="d">Peek</span> from <span class="k">stack</span>
+
+    if (<span class="k">current</span> is connected to a node not in <span class="k">visitedNodes</span>) {
+      <span class="k">selected</span> <- <span class="d">Select</span> a random edge that joins <span class="k">current</span> and a <span class="k">newNode</span> not in <span class="k">visitedNodes</span>
+
+      <span class="d">Add</span> <span class="k">newNode</span> to <span class="k">visitedNodes</span>
+      <span class="d">Push</span> <span class="k">newNode</span> onto <span class="k">stack</span>
+
+      <span class="d">Add</span> <span class="k">selected</span> to <span class="k">result</span>
+    } else {
+      <span class="d">Pop</span> from <span class="k">stack</span>
+    }
+  }
+
+  return <span class="k">result</span>
+}`;
+
 function App() {
   return (
     <Container className="mt-4 mb-4">
@@ -12,7 +77,7 @@ function App() {
         </h1>
         <section>
           <h2>Animations</h2>
-          <main>
+          <main className="d-flex flex-row flex-wrap justify-content-center">
             <section className="card h-fitc">
               <div className="card-body">
                 <h3 className="card-title">Prim's Algorithm</h3>
@@ -45,31 +110,62 @@ function App() {
         <section>
           <h2>Description</h2>
           <p>
-            Maze can be generated with spanning tree using graph nodes
-            representing grid.{' '}
+            A maze can be generated using a spanning tree, with graph nodes
+            representing a grid.{' '}
             <a href="https://en.wikipedia.org/wiki/Maze_generation_algorithm">
               Maze Generation Algorithm - wikipedia
             </a>
           </p>
           <p>
-            Some algorithms to find minimum spanning tree are Prim's algorithm
-            and Kruskal's algorithm. Two of the examples are minimum spanning
-            tree maze of random weights with the algorithms. You can see the
-            difference of generation process.
+            Some algorithms for finding a <dfn>minimum spanning tree</dfn> (
+            <abbr>MST</abbr>) include Prim's algorithm and Kruskal's algorithm.
+            Two examples are generating a minimum spanning tree maze with random
+            weights using these algorithms. You can see the differences in the
+            generation process.
           </p>
           <p>
-            In addition, spanning tree can be found by random DFS with no loop.
-            You can see the process of DFS and compare its result with minimum
-            spanning tree algorithms'.
+            Additionally, a spanning tree can be found using a random{' '}
+            <dfn>Depth-First Search</dfn> (<abbr>DFS</abbr>) without loops. You
+            can observe the <abbr>DFS</abbr> process and compare its results
+            with those of minimum spanning tree.
           </p>
         </section>
-        <section>
+        <section className="d-flex flex-column">
           <h2>Implementation</h2>
           <section className="card">
             <div className="card-body">
               <h3 className="card-title" id="prim-details">
                 Prim's Algorithm
               </h3>
+              <p className="card-text">
+                In Prim's algorithm, a tree is grown to become an{' '}
+                <abbr>MST</abbr>.
+              </p>
+              <ol className="card-text">
+                <li>Start from an arbitrary node.</li>
+                <li>Add this node to a set.</li>
+                <ol>
+                  <li>
+                    Select the minimum-weight edge that connects a node in the
+                    set and a new node.
+                  </li>
+                  <li>Add the new node to the set.</li>
+                </ol>
+                <li>
+                  Repeat this selection process until every node is included in
+                  the set.
+                </li>
+              </ol>
+              <figure>
+                <figcaption>Pseudo Code</figcaption>
+                <pre>
+                  <code
+                    dangerouslySetInnerHTML={{
+                      __html: pseudoPrim,
+                    }}
+                  ></code>
+                </pre>
+              </figure>
             </div>
           </section>
           <section className="card">
@@ -77,13 +173,80 @@ function App() {
               <h3 className="card-title" id="kruskal-details">
                 Kruskal's Algorithm
               </h3>
+              <p className="card-text">
+                In Kruskal's algorithm, several trees are grown and merged to
+                form an <abbr>MST</abbr>.
+              </p>
+              <ol className="card-text">
+                <li>Initialize an empty disjoint set forest.</li>
+                <li>Sort all edges in non-decreasing order of weight.</li>
+                <li>
+                  For each edges in order,
+                  <ul>
+                    <li>
+                      If the two nodes of the edge are not in the same tree, do
+                      the following:
+                    </li>
+                    <ol>
+                      <li>Select the edge.</li>
+                      <li>Add the two nodes to the forest.</li>
+                    </ol>
+                  </ul>
+                </li>
+              </ol>
+              <figure>
+                <figcaption>Pseudo Code</figcaption>
+                <pre>
+                  <code
+                    dangerouslySetInnerHTML={{
+                      __html: pseudoKruskal,
+                    }}
+                  ></code>
+                </pre>
+              </figure>
             </div>
           </section>
           <section className="card">
             <div className="card-body">
               <h3 className="card-title" id="dfs-details">
-                stack DFS
+                no loop <abbr>DFS</abbr>
               </h3>
+              <p className="card-text">
+                <abbr>DFS</abbr> can be used to create or solve a maze in a
+                manner similar to navigating through a maze room.
+              </p>
+              <ol className="card-text">
+                <li>Start from an arbitrary node.</li>
+                <ul>
+                  <li>
+                    If there are any edges connecting the current node to a new
+                    node, do the following:
+                  </li>
+                  <ol>
+                    <li>Randomly select an edge from those edges.</li>
+                    <li>Start over from that new node.</li>
+                  </ol>
+                  <li>If not, do the following:</li>
+                  <ol>
+                    <li>Go back to the previous node.</li>
+                  </ol>
+                </ul>
+                <li>Repeat these steps until there is no previous node.</li>
+              </ol>
+              <p className="card-text">
+                You can implement this with stack to avoid excessively deep
+                recursion.
+              </p>
+              <figure>
+                <figcaption>Pseudo Code</figcaption>
+                <pre>
+                  <code
+                    dangerouslySetInnerHTML={{
+                      __html: pseudoDFS,
+                    }}
+                  ></code>
+                </pre>
+              </figure>
             </div>
           </section>
         </section>
